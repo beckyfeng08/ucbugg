743509c0ba044f5fb711fcf8028b6d0b1f9ace32
saurabhmitra <saurabh.mitra@berkeley.edu>
2016-06-18 16:14:17 -0700

    did a whole buncha shit

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,8 @@ var Main = (function() {
 		$("#home-info").hide(switchSpeed);
 		$("#about-info").show(switchSpeed);
 		$("#about-facilitators").show(switchSpeed);
-		$("#home").style.backgroundColor = "transparent"
+		// $("#home").style.backgroundColor = "transparent"
+	   	$("#blue-box").animate({left:'130px'}, switchSpeed);
 	}
 	var loadHome = function() {
 		var mainbody = document.getElementById('mainbody'); //jquery failed me
@@ -22,16 +23,54 @@ var Main = (function() {
 		$("#home-info").show(switchSpeed);
 		$("#about-info").hide(switchSpeed);
 		$("#about-facilitators").hide(switchSpeed);
-		$("#about").style.backgroundColor = "transparent"
+		// $("#about").style.backgroundColor = "transparent"
+	   	$("#blue-box").animate({left:'2px'}, switchSpeed);
+	}
+
+	var scheduleHover = function() {
+		$("#schedule-button").animate({width:"175px"}, switchSpeed/1.5);
+		$("#schedule-text").show(switchSpeed/1.5);
+	}
+	var scheduleOut = function() {
+		$("#schedule-button").animate({width:"60px"}, switchSpeed/1.5);
+		$("#schedule-text").hide(switchSpeed/1.5);
+	}
+	var logisticsHover = function() {
+		$("#logistics-button").animate({width:"175px"}, switchSpeed/1.5);
+		$("#logistics-text").show(switchSpeed/1.5);
+		
+	}
+	var logisticsOut = function() {
+		$("#logistics-button").animate({width:"60px"}, switchSpeed/1.5);
+		$("#logistics-text").hide(switchSpeed/1.5);
+	}
+	var piazzaHover = function() {
+		$("#piazza-button").animate({width:"145px"}, switchSpeed/1.5);
+		$("#piazza-text").show(switchSpeed/1.5);
+	}
+	var piazzaOut = function() {
+		$("#piazza-button").animate({width:"60px"}, switchSpeed/1.5);
+		$("#piazza-text").hide(switchSpeed/1.5);
 	}
 
 	var start = function() {
 		loadHome();
+		$(".info-text").hide();
+		$(".info-button").width("50px");
 	};
 
 	return {
 		start: start,
+
 		about: loadAbout,
-		home: loadHome
+		home: loadHome,
+
+		scheduleHover: scheduleHover,
+		logisticsHover: logisticsHover,
+		piazzaHover: piazzaHover,
+
+		scheduleOut: scheduleOut,
+		logisticsOut: logisticsOut,
+		piazzaOut: piazzaOut
 	}
 })();
