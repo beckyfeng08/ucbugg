4e016bb3970513c16d198476f5ca7980b58427b8
Saurabh Mitra <saurabh.mitra@berkeley.edu>
2016-08-09 22:23:34 -0700

    began basic projects page

---
+++ static/scripts/projects-processor.js
@@ -0,0 +1,17 @@
+var screenshots = {
+	"2tailz.png": .5,
+	"dailycommute.png": .7,
+	"generousghost.png":.4,
+	"herebedragons.png": .8,
+	"penguins.png": .5,
+	"sidewalkshuffle.png": .3,
+	"stars.png": .4,
+	"sweetvictory.png": .5,
+	"watercycle.png": .4,
+	"worlddomination.png": .3
+}
+
+var updateProjects = function() {
+	var keys = Object.keys(screenshots);
+
+}
