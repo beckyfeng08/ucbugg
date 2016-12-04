3d4ab7aed2c43cfd880e116756c0ba14f57cceb9
Saurabh Mitra <saurabhmitra@fb.com>
2016-12-03 19:24:01 -0800

    js fix

--- update_GAE.sh
+++ update_GAE.sh
@@ -1,1 +1,1 @@
-python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v6 update --no_cookies ./
+python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v69 update --no_cookies ./
