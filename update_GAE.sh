2120aea3136f2ca3292a9a937215c73716991711
Veronica Chen <veronicaa.chen@yahoo.com>
2016-12-31 19:03:23 -0800

    fixed pizza link

--- update_GAE.sh
+++ update_GAE.sh
@@ -1,1 +1,1 @@
-python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v73 update --no_cookies ./
+python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v75 update --no_cookies ./
