706509d93044ad7e209e6d0385e9d91e6de4901f
Saurabh Mitra <saurabhmitra@fb.com>
2017-04-22 21:15:37 -0700

    fixed rigging lab shit

--- update_GAE.sh
+++ update_GAE.sh
@@ -1,1 +1,1 @@
-python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v77 update --no_cookies ./
+python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v79 update --no_cookies ./
