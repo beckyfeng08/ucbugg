7d1751107ec8eed0a7d0ab3d24efd74eefc36414
Ricky Herrera <rherrera@berkeley.edu>
2018-03-12 12:49:16 -0700

    hw 7 even tho its almost hw8

--- update_GAE.sh
+++ update_GAE.sh
@@ -1,1 +1,1 @@
-python ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v81 update --no_cookies ./
+python2.7 ~/Downloads/google_appengine/appcfg.py -A ucbugg-flask -V v81 update --no_cookies ./
